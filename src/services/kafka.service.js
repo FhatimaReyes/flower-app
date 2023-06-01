98dc6de1a14379663344ceb846b81bc40a355231
FhatimaReyes <fhareal10@gmail.com>
2023-06-01 09:51:55 -0600

    add kafka-front

--- src/services/kafka.service.js
+++ src/services/kafka.service.js
@@ -1,23 +1,39 @@
 class KafkaService {
-  //url = 'https://your-kafka-express-service-kafka-adsoftsito.cloud.okteto.net/';
-  url = 'https://kafka-express-service-fhatimareyes.cloud.okteto.net/';
- 
-  reaction = async (name) => {
-   await fetch(this.url + 'like?name=' + name, {
-      method: 'GET',
-      headers: {
-         'Content-type': 'application/json; charset=UTF-8',
-      },  
-   })  
-      .then((response) => console.log(response.json()))
-      .then((data) => {
-        console.log(data);
-      })  
-      .catch((err) => {
-         console.log(err.message);
-      }); 
-  }
+   url = 'https://kafka-express-service-fhatimareyes.cloud.okteto.net/';
+  
+   reactionPush = async (data) => {
+       await fetch(this.url + 'reaction?userId=' + data.userId + '&objectId=' + data.objectId + '&reactionId=' + data.reactionId  , {
+           method: 'GET',
+           headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+           },
+       })
+           .then((response) => console.log(response.json()))
+           .then((data) => {
+               console.log(data);
+           })
+           .catch((err) => {
+               console.log(err.message);
+           });
+   }
+
+   commentPush = async (data) => {
+       await fetch(this.url + 'comments?userId=' + data.userId + '&objectId=' + data.objectId + '&comment=' + data.comment  , {
+           method: 'GET',
+           headers: {
+               'Content-type': 'application/json; charset=UTF-8',
+           },
+       })
+           .then((response) => console.log(response.json()))
+           .then((data) => {
+               console.log(data);
+           })
+           .catch((err) => {
+               console.log(err.message);
+           });
+   }
 
 }
-// eslint-disable-next-line
-export default new KafkaService();
+
+const KafkaServiceInstance = new KafkaService();
+export default KafkaServiceInstance;
